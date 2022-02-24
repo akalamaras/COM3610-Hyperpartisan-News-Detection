c4ecb2f371c3a93c27399bcbb96a2108400f44f6
Alexandros Kalamaras <a.kalamaras@outlook.com>
2022-02-24 19:03:19 +0000

    Created preprocessing and process_xml files

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -3,5 +3,12 @@ Author: Alexandros Kalamaras
 Date: 23 February 2022
 '''
 
+# Clean XML https://github.com/GateNLP/semeval2019-hyperpartisan-bertha-von-suttner/blob/4b1d74b73247a06ed79e8e7af30923ce6828574a/Preprocessing/preprocessing.py
 def clean(text):
+	pass
+
+def tokenize(text):
+	pass
+
+def remove_stopwords(text):
 	pass
---
+++ preprocessing/process_xml.py
@@ -0,0 +1,7 @@
+'''
+Author: Alexandros Kalamaras
+'''
+
+# https://pandas.pydata.org/docs/reference/api/pandas.read_xml.html
+def to_dataframe(xml_path):
+	pass
